418729e174bf98de26d65dd92a6b97bfabee32f9
RylanDS7 <ryds105@gmail.com>
2026-05-28 16:35:21 -0700

    Finer mesh and better figure plotting

--- plot_mt_results.py
+++ plot_mt_results.py
@@ -38,7 +38,12 @@ phase_yx = data[:, 3, :]
 
 plot_freqs_ind = [0, 10, 20, 30, 40, 50]
 
-x_cut = rx_locs[0:45, 0]
+x_cut = rx_locs[22::45, 0] # cut along y=0
+
+# ======================
+# Apparent resisitivty and phase plots for individual freqs
+# ======================
+
 labels = ['App Res xy', 'Phase xy', 'App Res yx', 'Phase yx']
 
 for j in plot_freqs_ind:
@@ -49,14 +54,14 @@ for j in plot_freqs_ind:
     print(f"Frequency {freqs[j]} Hz:")
 
     for i, (ax, label) in enumerate(zip(axes, labels)):
-        ax.plot(x_cut, data[j, i, :], '.-', label="Simulated")
+        ax.plot(x_cut, data[j, i, 22::45], '.-', label="Simulated")
         ax.plot(x_cut, Adata[j, :, i], '.-', label="Analytic")
         ax.set_title(label)
         ax.set_xlabel('Easting (m)')
         if i % 2 == 0:
             ax.set_ylabel('App Res (Ωm)')
-            ax.plot(x_cut, data[j, i, :]-Adata[j, :, i], '.-', label="Residual: Sim - Analytic")
-            print(f"{label}:     Max residual: {max(np.abs(data[j, i, :]-Adata[j, :, i])) :.3}       Squared Mean: {np.mean(np.square(np.abs(data[j, i, :]-Adata[j, :, i]))) :.3}")
+            ax.plot(x_cut, data[j, i, 22::45]-Adata[j, :, i], '.-', label="Residual: Sim - Analytic")
+            print(f"{label}:     Max residual: {max(np.abs(data[j, i, 22::45]-Adata[j, :, i])) :.3}       Squared Mean: {np.mean(np.square(np.abs(data[j, i, 22::45]-Adata[j, :, i]))) :.3}")
         else:
             ax.set_ylabel('Phase (Degrees)')
         ax.grid(True, which='both', alpha=0.3)
@@ -64,4 +69,88 @@ for j in plot_freqs_ind:
 
     plt.suptitle(f'Apparent Resistivity and Phase along Cut at y=0 for {freqs[j]}Hz')
     plt.tight_layout()
-    plt.show()
+    plt.savefig(f"figure_out/compare{freqs[j]}Hz.png")
+
+
+
+# ======================
+# Apparent resisitivty and phase xy
+# ======================
+
+fig, axes = plt.subplots(1, 2, figsize=(16, 8), sharex=True)
+axes = axes.flatten()
+
+for i in plot_freqs_ind:
+    axes[0].plot(x_cut, data[i, 0, 22::45], '.-', label=f"Simulated {freqs[i]}Hz")
+    axes[0].plot(x_cut, Adata[i, :, 0], '.-', label=f"Analytic {freqs[i]}Hz")
+    axes[1].plot(x_cut, data[i, 1, 22::45], '.-', label=f"Simulated {freqs[i]}Hz")
+    axes[1].plot(x_cut, Adata[i, :, 1], '.-', label=f"Analytic {freqs[i]}Hz")
+
+axes[0].set_title("Apparent Resisitivy xy")
+axes[0].legend()
+axes[1].set_title("Phase xy")
+axes[1].legend()
+plt.suptitle("xy Apparent Resistivity and Phase along Cut at y=0")
+plt.savefig("figure_out/xyCompare.png")
+
+
+
+# ======================
+# Apparent resisitivty and phase residuals xy
+# ======================
+
+fig, axes = plt.subplots(1, 2, figsize=(16, 8), sharex=True)
+axes = axes.flatten()
+
+for i in plot_freqs_ind:
+    axes[0].plot(x_cut, data[i, 0, 22::45]-Adata[i, :, 0], '.-', label=f"Residuals {freqs[i]}Hz")
+    axes[1].plot(x_cut, data[i, 1, 22::45]-Adata[i, :, 1], '.-', label=f"Residuals {freqs[i]}Hz")
+
+axes[0].set_title("Apparent Resisitivy xy")
+axes[0].legend()
+axes[1].set_title("Phase xy")
+axes[1].legend()
+plt.suptitle("xy Apparent Resistivity and Phase Residuals along Cut at y=0")
+plt.savefig("figure_out/xyResiduals.png")
+
+
+
+# ======================
+# Apparent resisitivty and phase yx
+# ======================
+
+fig, axes = plt.subplots(1, 2, figsize=(16, 8), sharex=True)
+axes = axes.flatten()
+
+for i in plot_freqs_ind:
+    axes[0].plot(x_cut, data[i, 2, 22::45], '.-', label=f"Simulated {freqs[i]}Hz")
+    axes[0].plot(x_cut, Adata[i, :, 2], '.-', label=f"Analytic {freqs[i]}Hz")
+    axes[1].plot(x_cut, data[i, 3, 22::45], '.-', label=f"Simulated {freqs[i]}Hz")
+    axes[1].plot(x_cut, Adata[i, :, 3], '.-', label=f"Analytic {freqs[i]}Hz")
+
+axes[0].set_title("Apparent Resisitivy yx")
+axes[0].legend()
+axes[1].set_title("Phase yx")
+axes[1].legend()
+plt.suptitle("yx Apparent Resistivity and Phase along Cut at y=0")
+plt.savefig("figure_out/yxCompare.png")
+
+
+
+# ======================
+# Apparent resisitivty and phase residuals yx
+# ======================
+
+fig, axes = plt.subplots(1, 2, figsize=(16, 8), sharex=True)
+axes = axes.flatten()
+
+for i in plot_freqs_ind:
+    axes[0].plot(x_cut, data[i, 2, 22::45]-Adata[i, :, 2], '.-', label=f"Residuals {freqs[i]}Hz")
+    axes[1].plot(x_cut, data[i, 3, 22::45]-Adata[i, :, 3], '.-', label=f"Residuals {freqs[i]}Hz")
+
+axes[0].set_title("Apparent Resisitivy yx")
+axes[0].legend()
+axes[1].set_title("Phase yx")
+axes[1].legend()
+plt.suptitle("yx Apparent Resistivity and Phase Residuals along Cut at y=0")
+plt.savefig("figure_out/yxResiduals.png")
