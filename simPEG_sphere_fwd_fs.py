84c4470709d7691bf0ae18eb5042babdced56faa
RylanDS7 <ryds105@gmail.com>
2026-06-17 15:38:20 -0700

    Deeper receiver mesh layer refinement

--- simPEG_sphere_fwd_fs.py
+++ simPEG_sphere_fwd_fs.py
@@ -17,7 +17,7 @@ from matplotlib.colors import LogNorm
 
 import time
 
-run_num = "003" # for saving outputs to different folders
+run_num = "004" # for saving outputs to different folders
 
 # ======================================
 # Define receiver locations
@@ -92,7 +92,7 @@ mesh.refine_box(
 
 # Finer refinement around the rxs surface
 mesh.refine_box(
-    [-5000, -5000, -25],
+    [-5000, -5000, -50],
     [5000, 5000, 0],
     levels=-1,
     finalize=False
